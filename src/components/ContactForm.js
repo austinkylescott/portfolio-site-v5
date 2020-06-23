564e98595bfe10d6d84c730656fcb5022427c1fb
Austin Scott <austin.scott18@gmail.com>
2020-06-23 18:15:58 -0400

    Test contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -25,7 +25,7 @@ export default function ContactForm() {
         fetch("/", {
           method: "POST",
           headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: encode({ "form-name": "contact-demo", ...values }),
+          body: encode({ "form-name": "contact-form", ...values }),
         })
           .then(() => {
             alert("Success")
