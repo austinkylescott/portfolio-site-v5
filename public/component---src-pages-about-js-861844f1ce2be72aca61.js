73e9a79e3aaf01b854050501ec94578f99b9b65c
Austin Scott <austin.scott18@gmail.com>
2020-06-22 23:49:22 -0400

    build to netlify

---
+++ public/component---src-pages-about-js-861844f1ce2be72aca61.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"3XHS":function(n,e,t){"use strict";t.r(e),t.d(e,"default",(function(){return r}));var a=t("q1tI"),c=t.n(a);function r(){return c.a.createElement("div",{className:"container page-container"},"ABOUT PAGE")}}}]);
+//# sourceMappingURL=component---src-pages-about-js-861844f1ce2be72aca61.js.map
