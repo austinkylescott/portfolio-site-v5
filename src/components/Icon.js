da90bdaae36e46c48d3a8987d5d97983376f5634
Austin Scott <austin.scott18@gmail.com>
2020-07-09 01:19:38 -0400

    attempt to fix netlify build bug

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -3,7 +3,12 @@ import HTML5Logo from "../icons/html5.svg"
 import BootstrapLogo from "../icons/bootstrap.svg"
 import CSS3Logo from "../icons/css3.svg"
 
-const iconTypes = { html5: HTML5Logo, bootstrap: BootstrapLogo, css3: CSS3Logo }
+const iconTypes = {
+  html5: HTML5Logo,
+  bootstrap: BootstrapLogo,
+  css3: CSS3Logo,
+  gatsby: gatsby.svg,
+}
 
 const IconComponent = ({ name, ...props }) => {
   let Icon = iconTypes[name]
