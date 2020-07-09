6fbab37f86155cd087057dfc8c16514be8ef55da
Austin Scott <austin.scott18@gmail.com>
2020-07-09 00:11:33 -0400

    attempt to fix gatsby build error

--- public/component---src-pages-about-js-f7ea09bdc27694e77bf7.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"3XHS":function(n,e,t){"use strict";t.r(e),t.d(e,"default",(function(){return r}));var a=t("q1tI"),c=t.n(a);function r(){return c.a.createElement("div",{className:"container page-container"},"ABOUT PAGE")}}}]);
-//# sourceMappingURL=component---src-pages-about-js-f7ea09bdc27694e77bf7.js.map
