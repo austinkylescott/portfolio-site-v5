63a12a00751312487d2be2f12ba1beb749307693
Austin Scott <austin.scott18@gmail.com>
2020-07-09 01:38:26 -0400

    Attempt to fix netlify build error

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -2,12 +2,13 @@ import React from "react"
 import HTML5Logo from "../icons/html5.svg"
 import BootstrapLogo from "../icons/bootstrap.svg"
 import CSS3Logo from "../icons/css3.svg"
+import GatsbyLogo from "../icons/gatsby.svg"
 
 const iconTypes = {
   html5: HTML5Logo,
   bootstrap: BootstrapLogo,
   css3: CSS3Logo,
-  gatsby: gatsby.svg,
+  gatsby: GatsbyLogo,
 }
 
 const IconComponent = ({ name, ...props }) => {
