5f8e8b2210fd93960605f557a228fc01ef36b69a
Austin Scott <austin.scott18@gmail.com>
2020-09-05 12:19:54 -0400

    attempt to fix react-helmet bug

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,27 +1,15 @@
 import React from "react"
-import { Helmet } from "react-helmet"
 import Header from "../components/Header"
 import "../scss/styles.scss"
 
 export default function Layout(props) {
   return (
-    <>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <meta
-          name="description"
-          content="Austin Scott is a talented full stack web developer available for hire. This is his portfolio of HTML, CSS, Javascript, and React projects. Contact him to discuss a job or project. He wants to work with you."
-        />
-        <link rel="canonical" href="https://www.austinscott.dev" />
-        <title>Austin Scott, Web Developer</title>
-      </Helmet>
-      <div className="container page-container">
-        <Header />
-        <div className="page-content">
-          {props.children}
-          {/* <Footer /> */}
-        </div>
+    <div className="container page-container">
+      <Header />
+      <div className="page-content">
+        {props.children}
+        {/* <Footer /> */}
       </div>
-    </>
+    </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 import Footer from "../components/Footer"
 import Layout from "../components/Layout"
 import { Link } from "gatsby"
 import "../scss/styles.scss"
 
 const IndexPage = () => (
-  <Layout>
-    <React.Fragment>
+  <>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <meta
+        name="description"
+        content="Austin Scott is a talented full stack web developer available for hire. This is his portfolio of HTML, CSS, Javascript, and React projects. Contact him to discuss a job or project. He wants to work with you."
+      />
+      <link rel="canonical" href="https://www.austinscott.dev" />
+      <title>Austin Scott, Web Developer</title>
+    </Helmet>
+    <Layout>
       <h1>Hi, I'm Austin</h1>
       <p>
         I am a full-stack web developer and improvisational theater teacher
@@ -21,8 +31,8 @@ const IndexPage = () => (
         .
       </p>
       <Footer />
-    </React.Fragment>
-  </Layout>
+    </Layout>
+  </>
 )
 
 export default IndexPage
