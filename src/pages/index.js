97d6ee1499ffbe0d8f84eda24bc8d4a12667c545
Austin Scott <austin.scott18@gmail.com>
2020-09-04 23:26:37 -0400

    update meta tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,20 +6,22 @@ import "../scss/styles.scss"
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi, I'm Austin</h1>
-    <p>
-      I am a full-stack web developer and improvisational theater teacher based
-      in Gainesville, FL with an interest in building good looking and effective
-      websites.
-    </p>
-    <p>
-      You can check out <Link to="../work">my projects here</Link> or{" "}
-      <a href="https://www.github.com/austinkylescott">
-        view my github profile here
-      </a>
-      .
-    </p>
-    <Footer />
+    <React.Fragment>
+      <h1>Hi, I'm Austin</h1>
+      <p>
+        I am a full-stack web developer and improvisational theater teacher
+        based in Gainesville, FL with an interest in building good looking and
+        effective websites.
+      </p>
+      <p>
+        You can check out <Link to="../work">my projects here</Link> or{" "}
+        <a href="https://www.github.com/austinkylescott">
+          view my github profile here
+        </a>
+        .
+      </p>
+      <Footer />
+    </React.Fragment>
   </Layout>
 )
 
