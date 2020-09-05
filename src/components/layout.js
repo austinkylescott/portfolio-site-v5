0ef56aa39ce4cc63316d0aac771de1aa3d577fc5
Austin Scott <austin.scott18@gmail.com>
2020-09-04 22:57:38 -0400

    update meta tag

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,8 @@ export default function Layout(props) {
       <Helmet>
         <meta
           charSet="utf-8"
-          description="Austin Scott is a talented full stack web developer available for hire. This is his portfolio of HTML, CSS, Javascript, and React projects. Contact him to discuss a job or project. He wants to work with you."
+          name="description"
+          content="Austin Scott is a talented full stack web developer available for hire. This is his portfolio of HTML, CSS, Javascript, and React projects. Contact him to discuss a job or project. He wants to work with you."
         />
         <title>Austin Scott, Web Developer</title>
       </Helmet>
