dab15015ebefe791e8f04e511b73fb26a4dc3bc8
Austin Scott <austin.scott18@gmail.com>
2020-09-04 22:43:42 -0400

    add react-helmet for title

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,15 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 import Header from "../components/Header"
 import "../scss/styles.scss"
 
 export default function Layout(props) {
   return (
     <div className="container page-container">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Austin Scott, Web Developer</title>
+      </Helmet>
       <Header />
       <div className="page-content">
         {props.children}
